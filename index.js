70907e993ca24b6c852fa5cb2754f6f72ecf5222
itztiva <98614842+itztiva@users.noreply.github.com>
2024-02-08 13:48:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,40 +8,40 @@ client.once('ready', () => {
     const channelId = 'Channel_ID'; 
 
     const messageContent = {
-        "content": "", // message that the bot sends before the embed        
+        "content": " ", // message that the bot sends before the embed        
         "embeds": [  // example embed
             {
                 "color": 7477715,
                 "fields": [
                     {
-                        "name": "American 🇺🇸",
-                        "value": "Made By <@884509937076666368> - [Video](https://www.youtube.com/watch?v=NE_U_mjo5ME)"
+                        "name": " ",
+                        "value": " "
                     },
                     {
-                        "name": "French 🇫🇷",
-                        "value": "Made By <@351690433249804288> - [Video](https://www.youtube.com/watch?v=a-rCWUN-gNo&ab_channel=ByZNexus)"
+                        "name": " ",
+                        "value": " "
                     },
                     {
-                        "name": "Europe 🇪🇺",
-                        "value": "Made By <@746996581684871218> - [Video](https://www.youtube.com/watch?v=a1_028-5A4E)"
+                        "name": " ",
+                       "value": " "
                     },
                     {
-                        "name": "Estonian 🇪🇪",
-                        "value": "Made By <@804777454581448704> - [Video](https://www.youtube.com/watch?v=gt0UrgywksM)"
+                           "name": " ",
+                        "value": " "
                     }
                 ],
                 "author": {
-                    "name": "Astro Tutorials"
+                    "name": " "
                 },
                 "footer": {
-                    "text": "Enjoy! - Astro",
-                    "icon_url": "https://media.discordapp.net/attachments/1190820083262570576/1203627153334337626/pngggg-removebg-preview.png?ex=65d1c823&is=65bf5323&hm=b3fafff6dc92d4f631d36be2fcf2f096ca2db05137bfe71a9b3a3e88db26ae68&"
+                    "text": " ",
+                    "icon_url": " "
                 },
                 "image": {
-                    "url": "https://backend.ploosh.dev:2053/assets/AstroBanner.png"
+                    "url": " "
                 },
                 "thumbnail": {
-                    "url": "https://media.discordapp.net/attachments/1190820083262570576/1203627153334337626/pngggg-removebg-preview.png?ex=65d1c823&is=65bf5323&hm=b3fafff6dc92d4f631d36be2fcf2f096ca2db05137bfe71a9b3a3e88db26ae68&"
+                    "url": " "
                 }
             }
         ],
